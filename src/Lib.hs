ca608a992b6428016e705f954595c046a77b7bd4
ValentinOcchiuzzi <occhiuzzivalentin@gmail.com>
2025-04-28 18:42:35 -0300

    Braulio estrella terminado

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,35 +2,55 @@ module Lib () where
 
 import Text.Show.Functions ()
 
-doble :: Int -> Int
-doble x = x * 2
-
+type Poder = Personaje -> Personaje
 data Personaje = Personaje {
     nombre :: String,
     poderBasico :: Poder,
     superPoder :: Poder,
     superActiva :: Bool,
     vida :: Int
-}
+} deriving Show
 
-type Poder = Personaje -> Personaje
-lluviaDeTuercas :: Poder
-torretaCurativa :: Poder
+--PODERES
 bolaEspinosa :: Poder
-granadaDeEspinas :: Poder
+bolaEspinosa contrincante = contrincante {vida = max (vida contrincante - 1000) 0} 
 
-lluviaDeTuercas unPersonaje = unPersonaje
+lluviaDeTuercas :: String -> Personaje -> Personaje
+lluviaDeTuercas "sanadora" unPersonaje = unPersonaje {vida = vida unPersonaje + 800}
+lluviaDeTuercas "dañina" unPersonaje = unPersonaje {vida = div (vida unPersonaje) 2}
+  
+torretaCurativa :: Poder
+torretaCurativa aliado = aliado {vida = 2*vida aliado, superActiva = True}
 
-torretaCurativa unPersonaje = unPersonaje
+granadaDeEspinas :: Int -> Personaje -> Personaje
+granadaDeEspinas radio contrincante
+  |radio > 3 && estaEnLasUltimas contrincante = espinasEstuvoAqui contrincante {superActiva = False, vida = 0}
+  |radio > 3 = espinasEstuvoAqui contrincante 
+  |otherwise = bolaEspinosa contrincante
 
-bolaEspinosa unPersonaje = unPersonaje
+espinasEstuvoAqui :: Poder
+espinasEstuvoAqui personaje = personaje {nombre = nombre personaje ++ " Espinas estuvo aqui"}
 
-granadaDeEspinas unPersonaje = unPersonaje
+estaEnLasUltimas :: Personaje -> Bool
+estaEnLasUltimas unPersonaje = vida unPersonaje < 800
+
+--REPORTES
+atacarConPoderEspecial :: Personaje -> Personaje -> Personaje
+atacarConPoderEspecial unPersonaje contrincante
+  | superActiva unPersonaje = poderBasico unPersonaje . superPoder unPersonaje $ contrincante
+  | otherwise = contrincante
+
+estanEnLasUltimas :: [Personaje] -> [String]
+estanEnLasUltimas personajes = map nombre (filter estaEnLasUltimas personajes)
+
+--PERSONAJES
+personajes :: [Personaje]
+personajes = [pamela, espinas]
 
 pamela :: Personaje
 pamela = Personaje {
     nombre = "Pamela",
-    poderBasico = lluviaDeTuercas,
+    poderBasico = lluviaDeTuercas "sanadora",
     superPoder = torretaCurativa,
     superActiva = False,
     vida = 9600
@@ -40,7 +60,7 @@ espinas :: Personaje
 espinas = Personaje {
     nombre = "Espinas",
     poderBasico = bolaEspinosa,
-    superPoder = granadaDeEspinas,
+    superPoder = granadaDeEspinas 5,
     superActiva = True,
     vida = 4800
 }
