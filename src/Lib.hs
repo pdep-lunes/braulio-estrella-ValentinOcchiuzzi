660801e15fe81b91a2719da30eb2a94c581d8324
ValentinOcchiuzzi <occhiuzzivalentin@gmail.com>
2025-04-28 12:18:37 -0300

    Personajes creados, falta poderes

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,3 +4,43 @@ import Text.Show.Functions ()
 
 doble :: Int -> Int
 doble x = x * 2
+
+data Personaje = Personaje {
+    nombre :: String,
+    poderBasico :: Poder,
+    superPoder :: Poder,
+    superActiva :: Bool,
+    vida :: Int
+}
+
+type Poder = Personaje -> Personaje
+lluviaDeTuercas :: Poder
+torretaCurativa :: Poder
+bolaEspinosa :: Poder
+granadaDeEspinas :: Poder
+
+lluviaDeTuercas unPersonaje = unPersonaje
+
+torretaCurativa unPersonaje = unPersonaje
+
+bolaEspinosa unPersonaje = unPersonaje
+
+granadaDeEspinas unPersonaje = unPersonaje
+
+pamela :: Personaje
+pamela = Personaje {
+    nombre = "Pamela",
+    poderBasico = lluviaDeTuercas,
+    superPoder = torretaCurativa,
+    superActiva = False,
+    vida = 9600
+}
+
+espinas :: Personaje
+espinas = Personaje {
+    nombre = "Espinas",
+    poderBasico = bolaEspinosa,
+    superPoder = granadaDeEspinas,
+    superActiva = True,
+    vida = 4800
+}
